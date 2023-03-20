31f5d3a94c95d1227edcf8e11d3c73296b00c9d6
vzi00150 <vzi00150@gmail.com>
2023-03-20 16:46:27 +0900

    認可機能追加

--- src/main/java/com/techacademy/service/UserDetail.java
+++ src/main/java/com/techacademy/service/UserDetail.java
@@ -2,8 +2,10 @@ package com.techacademy.service;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.techacademy.entity.Employee;
@@ -12,11 +14,17 @@ public class UserDetail implements UserDetails {
     private static final long serialVersionUID = 1L;
 
     private final Employee employee;
-    private final Collection<? extends GrantedAuthority> authorities;
+    //private final Collection<? extends GrantedAuthority> authorities;
+    private final List<SimpleGrantedAuthority> authorities;
 
     public UserDetail(Employee employee) {
         this.employee = employee;
-        this.authorities = new ArrayList<GrantedAuthority>();
+        //this.authorities = new ArrayList<GrantedAuthority>();
+
+        List<SimpleGrantedAuthority> authorities = new ArrayList<SimpleGrantedAuthority>();
+        authorities.add(new SimpleGrantedAuthority(employee.getAuthentication().getRole().toString()));
+        this.authorities = authorities;
+
     }
 
     public Employee getEmployee() {
